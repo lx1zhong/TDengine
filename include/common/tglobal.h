b3e4224873c931a36e5f876e12bf623065a35f57
lx1zhong <284584628@qq.com>
2023-09-15 00:04:52 +0800

    add TSZ and ADT-FSE algorithm

--- include/common/tglobal.h
+++ include/common/tglobal.h
@@ -156,6 +156,7 @@ extern double   tsFPrecision;
 extern double   tsDPrecision;
 extern uint32_t tsMaxRange;
 extern uint32_t tsCurRange;
+extern bool     tsIfAdtFse;
 extern char     tsCompressor[];
 
 // tfs
